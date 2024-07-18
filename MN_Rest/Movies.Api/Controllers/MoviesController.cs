0c54fa00dfefeacad8c6186166e01bc755effebf
kolotko111 <milosznowicki96@wp.pl>
2024-07-18 18:26:44 +0200

    base

---
+++ MN_Rest/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository = movieRepository;
+
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie.MapToResponse());
+    }
+    
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute]Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+        if (movie is null)
+        {
+            return NotFound();
+        }
+        return Ok(movie.MapToResponse());
+    }
+    
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await _movieRepository.GetAllAsync();
+
+        return Ok(movies.MapToResponse());
+    }
+}
---
+++ MN_Rest/Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies.Contracts.Responses;
+
+public record MoviesResponse
+{
+    public required IEnumerable<MovieResponse> Items { get; init; } = Enumerable.Empty<MovieResponse>();
+}
