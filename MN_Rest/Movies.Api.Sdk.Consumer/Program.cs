d8b4721a97c3bf8ee4a07d0df38c120d401da540
kolotko111 <milosznowicki96@wp.pl>
2024-08-01 18:07:28 +0200

    sdk

---
+++ MN_Rest/Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
+using Movies.Contracts.Requests;
+using Refit;
+
+var services = new ServiceCollection();
+// własny client factory, dodać pakiet
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s =>
+    {
+        async Task<string> AuthorizationHeaderValueGetter(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync();
+
+        return new RefitSettings
+        {
+            AuthorizationHeaderValueGetter = AuthorizationHeaderValueGetter
+        };
+    })
+    .ConfigureHttpClient(x =>
+        x.BaseAddress = new Uri("https://localhost:5001"));
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("test1-2020");
+var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+{
+    Title = "Spiderman 2",
+    YearOfRelease = 2002,
+    Genres = new []{ "Action"}
+});
+
+await moviesApi.UpdateMovieAsync(newMovie.Id, new UpdateMovieRequest()
+{
+    Title = "Spiderman 2",
+    YearOfRelease = 2002,
+    Genres = new []{ "Action", "Adventure"}
+});
+
+await moviesApi.DeleteMovieAsync(newMovie.Id);
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
