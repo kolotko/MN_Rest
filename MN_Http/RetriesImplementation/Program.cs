20473bb5eb7f1ef7e2e7bbe884a1c05ae93b6ca2
kolotko111 <milosznowicki96@wp.pl>
2024-08-07 18:45:49 +0200

    ResiliencePipeline

--- MN_Http/RetriesImplementation/Program.cs
+++ MN_Http/RetriesImplementation/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using Polly;
 using Polly.Contrib.WaitAndRetry;
+using Polly.Registry;
+using Polly.Retry;
 using RetriesImplementation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,24 @@ builder.Services.AddHttpClient("BestpracticePoli", client =>
 {
     client.BaseAddress = new Uri("http://localhost:64593");
 }).AddTransientHttpErrorPolicy(policyBuilder => policyBuilder.WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5)));
+
+builder.Services.AddResiliencePipeline("default", x =>
+{
+    x.AddRetry(new RetryStrategyOptions
+    {
+        ShouldHandle = new PredicateBuilder().Handle<Exception>(),
+        Delay = TimeSpan.FromSeconds(2),
+        MaxRetryAttempts = 2,
+        BackoffType = DelayBackoffType.Exponential,
+        UseJitter = true
+    })
+    .AddTimeout(TimeSpan.FromSeconds(30));
+});
+
+builder.Services.AddHttpClient("standardresiliencehandler", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:64593");
+}).AddStandardResilienceHandler();
 
 var app = builder.Build();
 
@@ -29,4 +50,29 @@ app.MapGet("/bestpracticepoli", async (IHttpClientFactory httpFactory) =>
     var x = response;
 });
 
+app.MapGet("/resiliencepipelineprovider", async (ResiliencePipelineProvider<string> pipelineProvider, IHttpClientFactory httpFactory) =>
+{
+    var client = httpFactory.CreateClient();
+    var pipeline = pipelineProvider.GetPipeline("default");
+    var response = await pipeline.ExecuteAsync( async ct =>
+    {
+        var response = await client.GetAsync("http://localhost:64593/test", ct);
+        if (response.StatusCode >= HttpStatusCode.InternalServerError)
+        {
+            throw new Exception();
+        }
+
+        return response;
+    });
+    var x = response;
+});
+
+app.MapGet("/standardresiliencehandler", async (IHttpClientFactory httpFactory) =>
+{
+    var client = httpFactory.CreateClient("standardresiliencehandler");
+    var response = await client.GetAsync("/test");
+    var x = response;
+});
+
+ 
 app.Run();
