1bffc1b58ff68efa1a1f22335f54eb8ddd9d77be
kolotko111 <milosznowicki96@wp.pl>
2024-08-15 09:45:20 +0200

    Json

---
+++ MN_Json/MN_Json/Person.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MN_Json;
+
+//jeśli będą nadmiarowe property w json to tutaj możesz je pominąć/ lub zabronić ich wystąpieniu
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool IsStudent { get; set; }
+    public DateTime Date { get; set; }
+}
+
+// wskaż nazwę pola jeśli inna niż properta
+public class Person2
+{
+    public string Name { get; set; }
+    [JsonPropertyName("age2")]
+    public int Age { get; set; }
+    [JsonPropertyName("isstudent")]
+    public bool IsStudent { get; set; }
+    public DateTime Date { get; set; }
+}
+
+//ignorowanie pól
+public class Person3
+{
+    public string Name { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public int Age { get; set; }
+    public bool IsStudent { get; set; }
+    [JsonIgnore]
+    public DateTime Date { get; set; }
+
+    public int AgeReadonly { get; private set; } = 30;
+}
---
+++ MN_Json/MN_Json/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using MN_Json;
+
+Person person = new Person
+{
+    Name = "Jan Kowalski",
+    Age = 30,
+    IsStudent = false,
+    Date = DateTime.Now
+};
+
+// serializacja
+string jsonString = JsonSerializer.Serialize(person);
+Console.WriteLine(jsonString);
+//deserializacja
+Person person2 = JsonSerializer.Deserialize<Person>(jsonString);
+
+// zapis do pliku
+using FileStream createStream = File.Create("Example.json");
+await JsonSerializer.SerializeAsync(createStream, person);
+
+// odczytanie jsona z komentarzami. Technicznie json nie może posiadac komentarzy. Oraz liczb zapisanych jako string
+var jsonWithCommentsString = File.ReadAllText("JsonFiles/JsonWithComments.json");
+
+var optionsAlowedCommentAndNumbersAsString = new JsonSerializerOptions
+{
+    AllowTrailingCommas = true,
+    ReadCommentHandling = JsonCommentHandling.Skip,
+    NumberHandling = JsonNumberHandling.AllowReadingFromString
+};
+
+Person person3 = JsonSerializer.Deserialize<Person>(jsonWithCommentsString, optionsAlowedCommentAndNumbersAsString);
+
+// Ignorowanie wielkich i małych liter. Domyślnie uwzględnia wielkość liter
+var jsonCaseInsensitive = File.ReadAllText("JsonFiles/JsonCaseInsensitive.json");
+
+var optionsCaseInsensitive = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    NumberHandling = JsonNumberHandling.AllowReadingFromString
+};
+
+var optionsCaseInsensitiveAlternative = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+Person person4 = JsonSerializer.Deserialize<Person>(jsonCaseInsensitive, optionsCaseInsensitive);
+Person person5 = JsonSerializer.Deserialize<Person>(jsonCaseInsensitive, optionsCaseInsensitiveAlternative);
+
+// Jeśli nie może znaleźć pola z obiektu wypełnia go domyślną wartością
+
+
+
+//przykład z innymi nazwami pól
+var jsonOtherFieldsName = File.ReadAllText("JsonFiles/JsonOtherFieldsName.json");
+Person2 person6 = JsonSerializer.Deserialize<Person2>(jsonOtherFieldsName);
+
+
+//ignorowanie niektórych pól i generowanie jsona z wcięciami, ignoruje kiedy private set i wartośc default
+Person3 personIgnore = new Person3
+{
+    Name = "Jan Kowalski",
+    Age = 30,
+    IsStudent = default,
+    Date = DateTime.Now
+};
+
+var optionsWriteIndented = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    IgnoreReadOnlyProperties = true,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+};
+
+string jsonString222 = JsonSerializer.Serialize(personIgnore, optionsWriteIndented);
